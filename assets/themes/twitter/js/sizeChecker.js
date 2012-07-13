42b548eea2c5f566cec0a7e113f7c50a8a3ebb0a
nanovazquez <mariano.vazquez@southworks.net>
2012-07-12 23:06:46 -0300

    fixed bug with aside in thinner resolution

--- assets/themes/twitter/js/sizeChecker.js
+++ assets/themes/twitter/js/sizeChecker.js
@@ -19,7 +19,7 @@ $(function(){
 	function checkBrowserWidth(){
 		var browserWidth = getBrowserWidth();
 		
-		if (browserWidth < 1117){
+		if (browserWidth < 1116){
 			$('body').addClass('thin'); 
 		}else{
 			$('body').removeClass('thin');
