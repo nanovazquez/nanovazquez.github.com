d3abe32acb1d362414780bc85c659d69ba16fe7f
Mariano Vazquez <mvazquez@mavasquez-ltm.internal.salesforce.com>
2019-05-17 09:02:01 -0300

    Delete old blog

--- old-blog/_posts/accessing-azure-role-environment-information-from-node/startup-task-sample/README.txt
+++
@@ -1,3 +0,0 @@
-To run this code you need to install the Windows Azure SDK for Node.js.
-
-You can download it from https://www.windowsazure.com/en-us/develop/nodejs/
--- old-blog/_posts/accessing-azure-role-environment-information-from-node/startup-task-sample/StartupTaskSample.Web/bin/set_azure_role_information.ps1
+++
@@ -1,9 +0,0 @@
-[Reflection.Assembly]::LoadWithPartialName("Microsoft.WindowsAzure.ServiceRuntime")
-[Environment]::SetEnvironmentVariable("RoleName", [Microsoft.WindowsAzure.ServiceRuntime.RoleEnvironment]::CurrentRoleInstance.Role.Name, "Machine") 
-[Environment]::SetEnvironmentVariable("RoleInstanceID", [Microsoft.WindowsAzure.ServiceRuntime.RoleEnvironment]::CurrentRoleInstance.Id, "Machine")
-[Environment]::SetEnvironmentVariable("RoleDeploymentID", [Microsoft.WindowsAzure.ServiceRuntime.RoleEnvironment]::DeploymentId, "Machine")
-[Environment]::SetEnvironmentVariable("IsAvailable", [Microsoft.WindowsAzure.ServiceRuntime.RoleEnvironment]::IsAvailable, "Machine") 
-[Environment]::SetEnvironmentVariable("CustomVariable", "Some value", "Machine")
-
-
-
